d0ac92b60bf9de356e31fe8ab638ab6aa90e4ed7
Cloud Dental Developer <express@dental.com>
2026-05-29 13:05:55 +0530

    Deploy web build to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1869607303" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "385832731" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
